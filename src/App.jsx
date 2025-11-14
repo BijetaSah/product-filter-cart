400dfc3f8c71fd4b9ebbd1eb1040a916eb59aeb4
BijetaSah <bijetasah@30gmail.com>
2025-11-14 22:00:11 +0530

    data: create data for products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="text-5xl">hello</div>;
+  return <div className="text-5xl"></div>;
 }
 
 export default App;
---
+++ src/features/products/productData.js
@@ -0,0 +1,124 @@
+import laptop from "./products/laptop.webp";
+import laptop2 from "./products/laptop-2.webp";
+// import laptop3 from "./products/laptop-3.webp";
+import atmoicHabits from "./products/atomic-habits.webp";
+import theCleanCode from "./products/the-clean-code.webp";
+import theGreatGatsby from "./products/the-great-gatsby.webp";
+import psychologyOfMoney from "./products/psychology-of-money.jpg";
+import hoodie from "./products/hoodie.webp";
+import jacket from "./products/jacket.webp";
+import tshirt from "./products/tshirt.webp";
+import jeans from "./products/jeans.webp";
+import headphones from "./products/headphones.webp";
+import phone from "./products/phone.webp";
+import sapiens from "./products/sapiens.jpg";
+import smartWatch from "./products/smart-watch.jpg";
+import sneakers from "./products/sneakers.jpg";
+
+export const products = [
+  {
+    id: 1,
+    name: "Laptop Pro 15",
+    category: "Electronics",
+    price: 1400,
+    image: laptop,
+  },
+  {
+    id: 2,
+    name: "The Great Gatsby",
+    category: "Books",
+    price: 15,
+    image: theGreatGatsby,
+  },
+  {
+    id: 3,
+    name: "Classic T-Shirt",
+    category: "Clothing",
+    price: 20,
+    image: tshirt,
+  },
+  {
+    id: 4,
+    name: "Smartphone Ultra",
+    category: "Electronics",
+    price: 899,
+    image: phone,
+  },
+  {
+    id: 5,
+    name: "Atomic Habits",
+    category: "Books",
+    price: 18,
+    image: atmoicHabits,
+  },
+  {
+    id: 6,
+    name: "Premium Hoodie",
+    category: "Clothing",
+    price: 45,
+    image: hoodie,
+  },
+  {
+    id: 7,
+    name: "Smartwatch Series 5",
+    category: "Electronics",
+    price: 250,
+    image: smartWatch,
+  },
+  {
+    id: 8,
+    name: "Sapiens: A Brief History",
+    category: "Books",
+    price: 22,
+    image: sapiens,
+  },
+  {
+    id: 9,
+    name: "Blue Denim Jeans",
+    category: "Clothing",
+    price: 55,
+    image: jeans,
+  },
+  {
+    id: 10,
+    name: "Gaming Laptop X",
+    category: "Electronics",
+    price: 1800,
+    image: laptop2,
+  },
+  {
+    id: 11,
+    name: "Clean Code",
+    category: "Books",
+    price: 30,
+    image: theCleanCode,
+  },
+  {
+    id: 12,
+    name: "Wireless Headphones",
+    category: "Electronics",
+    price: 199,
+    image: headphones,
+  },
+  {
+    id: 13,
+    name: "Casual Sneakers",
+    category: "Clothing",
+    price: 70,
+    image: sneakers,
+  },
+  {
+    id: 14,
+    name: "The Psychology of Money",
+    category: "Books",
+    price: 20,
+    image: psychologyOfMoney,
+  },
+  {
+    id: 15,
+    name: "Jacket",
+    category: "Clothing",
+    price: 25,
+    image: jacket,
+  },
+];
